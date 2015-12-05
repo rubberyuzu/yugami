b4c2c997737ff11dea318437484fa3f5c2ba7f2f
Yuzu Saijo <rubberyuzu@Yuzus-MacBook-Pro.local>
2015-12-05 16:57:52 +0900

    trying to update a widget

--- app/src/main/java/com/example/rubberyuzu/yugamiapp/clock.java
+++ app/src/main/java/com/example/rubberyuzu/yugamiapp/clock.java
@@ -1,9 +1,17 @@
 package com.example.rubberyuzu.yugamiapp;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.SystemClock;
+import android.util.Log;
 import android.widget.RemoteViews;
+
+import java.util.Calendar;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,6 +21,9 @@ public class clock extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
+
+        Log.i("notice", "onUpdate---------------------------------------------------------------");
+
         final int N = appWidgetIds.length;
         for (int i = 0; i < N; i++) {
             updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
@@ -23,6 +34,13 @@ public class clock extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+        final Intent intent = new Intent(context, yugami.class);
+        final PendingIntent pending = PendingIntent.getService(context, 0, intent, 0);
+        final AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarm.cancel(pending);
+//        long interval = 1000*60;
+        long interval = 1000*6;
+        alarm.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(),interval, pending);
     }
 
     @Override
@@ -33,7 +51,12 @@ public class clock extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        Log.i("notice", "coming to updateWidget yoyo---------------------------------------------------------------");
+//        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        Calendar c = Calendar.getInstance();
+        int seconds = c.get(Calendar.SECOND);
+        CharSequence widgetText = String.valueOf(seconds);
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock);
         views.setTextViewText(R.id.appwidget_text, widgetText);
