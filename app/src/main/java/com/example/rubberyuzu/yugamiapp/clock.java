0e55442fba8202e156bbe738bf2bbea206895908
Yuzu Saijo <rubberyuzu@Yuzus-MacBook-Pro.local>
2015-12-05 20:06:08 +0900

    widget updating every 5 sec

--- app/src/main/java/com/example/rubberyuzu/yugamiapp/clock.java
+++ app/src/main/java/com/example/rubberyuzu/yugamiapp/clock.java
@@ -2,8 +2,10 @@ package com.example.rubberyuzu.yugamiapp;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.nfc.Tag;
@@ -12,6 +14,7 @@ import android.util.Log;
 import android.widget.RemoteViews;
 
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
@@ -21,8 +24,6 @@ public class clock extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-
-        Log.i("notice", "onUpdate---------------------------------------------------------------");
 
         final int N = appWidgetIds.length;
         for (int i = 0; i < N; i++) {
@@ -34,13 +35,6 @@ public class clock extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
-        final Intent intent = new Intent(context, yugami.class);
-        final PendingIntent pending = PendingIntent.getService(context, 0, intent, 0);
-        final AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        alarm.cancel(pending);
-//        long interval = 1000*60;
-        long interval = 1000*6;
-        alarm.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(),interval, pending);
     }
 
     @Override
@@ -48,21 +42,66 @@ public class clock extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Enter relevant functionality for when the last widget is received
+        String action = intent.getAction();
+        Log.i("yuyu", "------coming to onReceive-------");
+        if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(action)) {
+            Intent i = new Intent(context, clock.class); // ReceivedActivityを呼び出すインテントを作成
+            i.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+
+
+            PendingIntent sender = PendingIntent.getBroadcast(context, 0, i, 0); // ブロードキャストを投げるPendingIntentの作成
+
+            Calendar calendar = Calendar.getInstance(); // Calendar取得
+            calendar.setTimeInMillis(System.currentTimeMillis()); // 現在時刻を取得
+            calendar.add(Calendar.SECOND, 5); // 現時刻より5秒後を設定
+
+//        AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE); // AlramManager取得
+            AlarmManager am = (AlarmManager)context.getSystemService(context.ALARM_SERVICE); // AlramManager取得
+
+            am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), sender);
+
+            ComponentName thisWidget = new ComponentName(context, clock.class);
+            AppWidgetManager manager = AppWidgetManager.getInstance(context);
+
+
+
+            Calendar c = Calendar.getInstance();
+            int seconds = c.get(Calendar.SECOND);
+            CharSequence widgetText = String.valueOf(seconds);
+
+            // Construct the RemoteViews object
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock);
+
+            views.setTextViewText(R.id.appwidget_text, widgetText);
+
+            int[] appWidgetIds = AppWidgetManager.getInstance(context).getAppWidgetIds(thisWidget);
+
+            // Instruct the widget manager to update the widget
+            manager.updateAppWidget(appWidgetIds[0], views);
+    
+
+            Log.i("yuyu", "------coming to update-------");
+        }
+        super.onReceive(context, intent);
+    }
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        Log.i("notice", "coming to updateWidget yoyo---------------------------------------------------------------");
-//        CharSequence widgetText = context.getString(R.string.appwidget_text);
+
         Calendar c = Calendar.getInstance();
         int seconds = c.get(Calendar.SECOND);
         CharSequence widgetText = String.valueOf(seconds);
 
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock);
+
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 }
-
