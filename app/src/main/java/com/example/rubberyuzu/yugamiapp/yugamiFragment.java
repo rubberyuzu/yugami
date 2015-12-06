aaddab351fa6ecbf183d62f9dd18fbd532217427
Yuzu Saijo <rubberyuzu@Yuzus-MacBook-Pro.local>
2015-12-06 11:01:10 +0900

    huge commit

--- app/src/main/java/com/example/rubberyuzu/yugamiapp/yugamiFragment.java
+++ app/src/main/java/com/example/rubberyuzu/yugamiapp/yugamiFragment.java
@@ -2,6 +2,7 @@ package com.example.rubberyuzu.yugamiapp;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,19 @@ public class yugamiFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_yugami, container, false);
+        RangeSeekBar<Integer> seekBar = new RangeSeekBar<Integer>(20, 75, getContext());
+        seekBar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener<Integer>() {
+            @Override
+            public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Integer minValue, Integer maxValue) {
+                // バーの値が変更された時に呼び出される
+                Log.d("yugami", "User selected new range values: MIN=" + minValue + ", MAX=" + maxValue);
+            }
+        });
+
+        View v = inflater.inflate(R.layout.fragment_yugami, container, false);
+// add RangeSeekBar to pre-defined layout
+        ViewGroup layout = (ViewGroup) v.findViewById(R.id.main_layout);
+        layout.addView(seekBar);
+        return v;
     }
 }
